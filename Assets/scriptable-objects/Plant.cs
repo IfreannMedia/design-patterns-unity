0760c57bb2966d00b0ee4dd90c5016507418bc04
Warren Roche <warrengamedev@gmail.com>
2022-09-21 15:20:28 +0200

    Updated scriptable objects
    kill player when they collide with high threat tree

--- Assets/scriptable-objects/Plant.cs
+++ Assets/scriptable-objects/Plant.cs
@@ -1,11 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Plant : MonoBehaviour
 {
 
-
     [SerializeField]
     private PlantData plant;
+    private SetPlantInfo spi;
+    private void Start()
+    {
+        spi = GameObject.FindWithTag("PlantInfo").GetComponent<SetPlantInfo>();
+    }
+
+    private void OnMouseDown()
+    {
+        spi.OpenPlantPanel();
+        spi.planeName.text = plant.Name;
+        spi.threatLevel.text = plant.Threat.ToString();
+        spi.plantIcon.GetComponent<RawImage>().texture = plant.Icon;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player" && plant.Threat == PlantData.THREAT.High)
+        {
+            PlayerControllerScrObj.dead = true;
+        }
+    }
 }
