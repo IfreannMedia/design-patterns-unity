792e5988d31761b00ad9e8d7f61656034c4c2a72
Warren Roche <warrengamedev@gmail.com>
2022-09-21 18:43:19 +0200

    reorganised folders, updated ECS code
    still needs to be rewritten for up to date ECS and hybrid renderer

--- Assets/fleyweight/ECS/SpawnCar.cs
+++ Assets/fleyweight/ECS/SpawnCar.cs
@@ -1,19 +1,17 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnCar : MonoBehaviour
 {
-
     public GameObject car;
     public GameObject camera;
 
-    private void Update()
+    void Update()
     {
-
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Vector3 pos = new Vector3(10,10,10);
+            Vector3 pos = new Vector3(10, 10, 10);
             GameObject c = Instantiate(car, pos, Quaternion.identity);
             camera.GetComponent<SmoothFollow>().target = c.transform;
         }
