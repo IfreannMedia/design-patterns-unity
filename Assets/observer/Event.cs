0cf5c08a99489f603f06b7a48d2382df17c21f36
Warren Roche <warrengamedev@gmail.com>
2022-09-22 18:46:30 +0200

    Added Event and EventListener, Egg Spawner

---
+++ Assets/observer/Event.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Event", menuName = "Game Event", order = 52)]
+public class Event : ScriptableObject
+{
+    private List<EventListener> eListeners = new List<EventListener>();
+
+    public void Register(EventListener l)
+    {
+        eListeners.Add(l);
+    }
+
+    public void UnRegister(EventListener l)
+    {
+        eListeners.Remove(l);
+    }
+
+    public void Occured()
+    {
+        for (int i = 0; i < eListeners.Count; i++)
+        {
+            eListeners[i].OnEventOccurs();
+        }
+    }
+}
