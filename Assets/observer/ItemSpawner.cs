4b85c68aa7869e0cbfdb812a48dfdb105b3ab5dc
Wario <37343942+IfreannMedia@users.noreply.github.com>
2022-09-27 22:17:54 +0100

    updated observer code
    added in pickup functionality, generalised the code a bit

--- Assets/observer/ItemSpawner.cs
+++ Assets/observer/ItemSpawner.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EggSpawner : MonoBehaviour
+public class ItemSpawner : MonoBehaviour
 {
 
     public GameObject eggPrefab;
+    public GameObject medpackPrefab;
     public Terrain terrain;
-    public Event eggDropp;
 
     private TerrainData terrainData;
 
@@ -15,8 +15,8 @@ public class EggSpawner : MonoBehaviour
     {
 
         terrainData = terrain.terrainData;
-        InvokeRepeating("CreateEgg", 1, 0.1f);
-
+        InvokeRepeating("CreateEgg", 1, 1f);
+        InvokeRepeating("CreateMedpack", 1, 0.1f);
     }
 
     private void CreateEgg()
@@ -26,7 +26,15 @@ public class EggSpawner : MonoBehaviour
         Vector3 pos = new Vector3(x, 0, z);
         pos.y = terrain.SampleHeight(pos) + 10;
         GameObject egg = Instantiate(eggPrefab, pos, Quaternion.identity);
-        eggDropp.Occured(egg);
+    }
+
+    private void CreateMedpack()
+    {
+        int x = (int)Random.Range(0, terrainData.size.x);
+        int z = (int)Random.Range(0, terrainData.size.z);
+        Vector3 pos = new Vector3(x, 0, z);
+        pos.y = terrain.SampleHeight(pos) + 10;
+        GameObject egg = Instantiate(medpackPrefab, pos, Quaternion.identity);
     }
 
 }
