78cf680624203ee2c13e48460dd2be4933132f4d
Wario <37343942+IfreannMedia@users.noreply.github.com>
2022-09-28 21:30:17 +0100

    Added the less performant ProcCube spawner

--- Assets/Prototype/ProcCube.cs
+++ Assets/Prototype/ProcCube.cs
@@ -128,5 +128,15 @@ public class ProcCube: Object
             i++; 
         }
 
+        cube.GetComponent<MeshFilter>().mesh = new Mesh();
+        cube.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);
+        cube.GetComponent<MeshFilter>().mesh.name = "Created Cube";
+        MeshRenderer rend = cube.GetComponent<MeshRenderer>();
+        rend.material = new Material(Shader.Find("Holistic/Plasma"));
+        cube.AddComponent<Rigidbody>();
+        cube.AddComponent<BoxCollider>();
+        cube.name = "Cube";
+        cube.gameObject.SetActive(true);
+        cube.transform.position = pos;
     }
 }
