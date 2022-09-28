6388350e6a86333ec30f17de8da0e49cdeffb0ee
Wario <37343942+IfreannMedia@users.noreply.github.com>
2022-09-28 21:47:29 +0100

    Updated prototype spawner to also sspawn spheres

--- Assets/Prototype/PrototypeCubeSpawner.cs
+++ Assets/Prototype/PrototypeCubeSpawner.cs
@@ -8,6 +8,9 @@ public class PrototypeCubeSpawner : MonoBehaviour
     void Update()
     {
         if (Random.Range(0, 100) < 10)
-            ProcCube.CreateCube(this.transform.position);
+            ProcCube.Clone(transform.position); // performant, cloning the originally created cube
+        else if (Random.Range(0, 100) < 10)
+            ProcSphere.Clone(transform.position);
+        // ProcCube.CreateCube(this.transform.position); - non performant, many individual cubes
     }
 }
