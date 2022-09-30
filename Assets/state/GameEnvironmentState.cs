ef4ee6860609b57f5a60d0480069611f05f945e2
Warren Roche <warrengamedev@gmail.com>
2022-09-30 18:52:07 +0200

    State module

---
+++ Assets/state/GameEnvironmentState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public sealed class GameEnvironmentState
+{
+    private static GameEnvironmentState instance;
+    private List<GameObject> checkpoints = new List<GameObject>();
+    public List<GameObject> Checkpoints { get { return checkpoints; } }
+
+    public static GameEnvironmentState Singleton
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameEnvironmentState();
+                instance.Checkpoints.AddRange(
+                    GameObject.FindGameObjectsWithTag("Checkpoint"));
+
+                instance.checkpoints = instance.checkpoints.OrderBy(waypoint => waypoint.name).ToList();
+            }
+            return instance;
+        }
+    }
+}
